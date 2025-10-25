8f55ced564ff1b53f245f52df52aea7fa8f9ab12
thendral <tamilthendral138@gmail.com>
2025-10-25 21:42:54 +0530

    initial commit

---
+++ src/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAnOonax1BLM7zEWNkad2EbRf96uj93cRk",
+    authDomain: "user-authentication-syst-d5067.firebaseapp.com",
+    projectId: "user-authentication-syst-d5067",
+    storageBucket: "user-authentication-syst-d5067.firebasestorage.app",
+    messagingSenderId: "206249557059",
+    appId: "1:206249557059:web:32acf472867d9c6c6e28f6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
